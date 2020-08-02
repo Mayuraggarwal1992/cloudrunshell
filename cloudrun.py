405f17f421f5e8bda8cddfdfe5bc0f90ad0f9551
ma185360 <Mayur.Aggarwal@ncr.com>
2020-08-02 15:33:55 +0530

    first commit

---
+++ cloudrun.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+from flask import Flask, request, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to cloud Run http Shell!"
+
+
+@app.route('/<string:command>')
+def cmd_to_run(command):
+    try:
+        cmd = command
+        cmd_output = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=10, universal_newlines=True)
+        response = make_response(cmd_output.stdout, 200)
+        response.mimetype = "text/plain"
+        return response
+    except subprocess.TimeoutExpired:
+        response = make_response("Timedout", 400)
+        response.mimetype = "text/plain"
+        return response
+    return redirect(url_for('home'))
+
+
+# @app.route('/exec', methods=['POST'])
+# def route_exec():
+#     # command = request.data.decode('utf-8')
+#     command = "dir -r"
+#     print(command)
+#     try:
+#         exec_output = subprocess.run('dir -r', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=10, universal_newlines=True)
+#         response = make_response(exec_output.stdout, 200)
+#         response.mimetype = "text/plain"
+#         return response
+#     except subprocess.TimeoutExpired:
+#         response = make_response("Timedout", 400)
+#         response.mimetype = "text/plain"
+#         return response
+#     return red
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
